442427a8da85a1d0fa0f943f2a969875dd08fdd8
SHAFAQUE-NAZ <shafaquenaz92@gmail.com>
2021-08-06 16:25:49 +0530

    changes merged

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@
 import './App.css';
 import Greet from './component/Greet';
 import Welcome from './component/Welcome';
-// import Hello from './component/Hello';
+import Hello from './component/Hello';
+import Message from './component/Message'
 
 function App() {
   return (
     <div className="App">
-      {<Greet />}
-      <Welcome />
+      <Greet name="Bruce" heroName = "Superman">
+      <p>This is props children
+      </p></Greet>
+      <Greet name="Clark" heroName = "Batman">
+      <button>Action</button>
+      </Greet>
+      <Greet name="Daina" heroName = "Wonder Women">
+      <input type = "text"></input>
+      </Greet>
+      <Welcome name="Bruce" heroName = "Superman"></Welcome>
+      <Welcome name="Clark" heroName = "Batman"></Welcome>
+      <Welcome name="Daina" heroName = "Wonder Women"></Welcome>
+      <Hello />
+      <Message />
       
     </div>
   );
