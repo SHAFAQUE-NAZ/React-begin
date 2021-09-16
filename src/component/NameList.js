16020259c089c4a3981eea14353da3f1a6c6e738
SHAFAQUE-NAZ <shafaquenaz92@gmail.com>
2021-09-16 23:10:08 +0530

    commit

--- src/component/NameList.js
+++ src/component/NameList.js
@@ -1,9 +1,31 @@
 import React from 'react'
 
 function NameList() {
-     const names = ["Bruce",'Diana','Clark']
-     const nameList = names.map(name => <h2>{name}</h2>)
-    return <div>{nameList}</div>
+    //  const names = ["Bruce",'Diana','Clark']
+    //  const nameList = names.map(name => <h2>{name}</h2>)
+    const person = [
+        {
+            id : 1,
+            name : 'Bruce',
+            age : 30,
+            skill : 'React'
+        },
+        {
+            id : 2,
+            name : 'Clark',
+            age : 28,
+            skill : 'Angular'
+        },
+        {
+            id : 3,
+            name : 'Diana',
+            age : 26,
+            skill : 'Vue'
+        }
+
+    ]
+    const personList = person.map(person => (<h2>I am {person.name}.I am {person.age} year old.I Know {person.skill}</h2>))
+    return <div>{personList}</div>
     
 }
 
